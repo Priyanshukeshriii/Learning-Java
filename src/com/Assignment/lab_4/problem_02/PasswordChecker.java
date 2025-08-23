04614b62cae4d19f574f0ce69b1cdcbb264057e4
S K Priyanshu <sk.priyanshukeshri@gmail.com>
2025-08-23 13:51:39 +0530

    Password checker

--- src/com/Assignment/lab_4/problem_02/PasswordChecker.java
+++ src/com/Assignment/lab_4/problem_02/PasswordChecker.java
@@ -1,4 +1,44 @@
 package com.Assignment.lab_4.problem_02;
+//We need to write the function to check the password entered is correct or not based on the following
+//conditions.
+//        a) It must have atleast one lower case character and one digit.
+//b) It must not have any Upper case characters and any special characters
+//c) length should be b/w 5-12.
+//d) It should not have any same immediate patterns like
+//abcanan1 : not acceptable coz of an an pattern
+//abc11se: not acceptable, coz of pattern 11
+//123sd123 : acceptable, as not immediate pattern
+//adfasdsdf : not acceptable, as no digits
+//Aasdfasd12: not acceptable, as have uppercase character
+
+
 
 public class PasswordChecker {
+    static boolean containsLowerCase(String password){
+        for (char ch : password.toCharArray()){
+            if(97<= (int) ch && (int)ch<= 122){
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean containsDigit(String password){
+        for (char ch : password.toCharArray()){
+            if(48<= (int) ch && (int)ch<= 57){
+                return true;
+            }
+        }
+        return false;
+    }
+    static boolean containsUpperCase(String password){
+        for (char ch : password.toCharArray()){
+            if(65<= (int) ch && (int)ch<= 96){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+
+    }
 }
